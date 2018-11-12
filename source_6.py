b5a83925e8768509c96b9f519a29bda7e2367efb
jBrewing <jbrewer256@gmail.com>
2018-11-12 12:24:43 -0700

    Done

--- source_6.py
+++ source_6.py
@@ -6,6 +6,9 @@
 # Description: These scripts will accomplish 2 things:
 #   Script 1. Make comparisons between weekday and weekend water
 #      consumption for Richards's Hall on USU's campus.
+#            - average hourly volume consumed with std. dev bars
+#            - boxplot for range, mean, and IQR
+#            - histogram for frequency distribution
 #   Script 2. Describe the impact of sampling period on the estimate of
 #      total water used over a time period.
 #__________________________________________________________________
@@ -21,15 +24,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
 # 2. read csv from Richards Hall and load data into pandas MAIN data frame
+# infer_datetime_format, parse_dates, and index_col combine to set date as index,
+# thereby enabling queries based on dates.
 df_main = pd.read_csv('datalog_Richards_Hall.csv', header =1, sep = ',',index_col=0,
                  parse_dates=True, infer_datetime_format=True, low_memory=False)
 
 
 
-# 3. Resample into hourly data
+# 3. Resample into hourly data with dataframe.resample.
+# .sum() sums incremental volume for each hourly resample.
 
 hourlyTotVol = df_main['IncrementalVolume'].resample(rule='1H', base = 0).sum()
+
 
 # 4. Use index.dayofweek to split main data frame into weekday and weekend
 
@@ -38,16 +46,14 @@ weekend_hourlyTotVol = hourlyTotVol[hourlyTotVol.index.dayofweek >=5]
 
 
 # 5. Aggregate into average hourly volume for weekday and weekend
-# with groupby function
+# with groupby function.  This averages the hourly consumption into a 24 hour frame for each day.
+# i.e. the average hourly consumption for ALL weekdays and the average hourly consumption for ALL
+# weekend days.
 
 # Week stats
 # Mean and std.dev
 week_hourlyAvgVol = week_hourlyTotVol.groupby(week_hourlyTotVol.index.hour).mean()
 week_hourlyStdVol = week_hourlyTotVol.groupby(week_hourlyTotVol.index.hour).std()
-
-# Max & min
-week_hourlyMaxVol = week_hourlyTotVol.max()
-week_hourlyMinVol = week_hourlyTotVol.min()
 
 
 # Weekend stats
@@ -55,36 +61,52 @@ week_hourlyMinVol = week_hourlyTotVol.min()
 weekend_hourlyAvgVol = weekend_hourlyTotVol.groupby(weekend_hourlyTotVol.index.hour).mean()
 weekend_hourlyStdVol = weekend_hourlyTotVol.groupby(weekend_hourlyTotVol.index.hour).mean()
 
-# Max and min
-weekend_hourlyMaxVol = weekend_hourlyTotVol.max()
-weekend_hourlyMinVol = weekend_hourlyTotVol.min()
 
 # 6. Plot average hourly weekday water demand vs average hourly weekend water demand
+# Plot as line with standard deviation bars.
 
-fig, ax = plt.subplots()
-
-plt.errorbar(x=week_hourlyAvgVol.index + 0.08, y=week_hourlyAvgVol,
+gridsize =(3,2)
+fig = plt.figure(figsize=(12,8))
+ax1 = plt.subplot2grid(gridsize, (0,0), colspan=2, rowspan=2)
+ax2 = plt.subplot2grid(gridsize, (2,0))
+ax3 = plt.subplot2grid(gridsize, (2,1))
+axis_font = {'fontname':'Arial', 'size':'9'}
+ax1.errorbar(x=week_hourlyAvgVol.index + 0.08, y=week_hourlyAvgVol,
              yerr=week_hourlyStdVol, capsize=3,
              capthick=0.5, fmt='--',
-             label='Weekday', marker='o')
+             label='Weekday (Mon-Fri)', marker='o')
 
-
-
-plt.errorbar(x = weekend_hourlyAvgVol.index, y = weekend_hourlyAvgVol,
+ax1.errorbar(x = weekend_hourlyAvgVol.index, y = weekend_hourlyAvgVol,
              yerr=weekend_hourlyStdVol, capsize=3,
              capthick=0.5, fmt='--',
-             label = 'Weekend', marker='o')
+             label = 'Weekend (Sat-Sun)', marker='o')
 
-ax.legend(loc='upper left')
-ax.set_title('Average Hourly Volumes - Richards Hall')
-ax.set_ylabel('Flow (gph)')
-ax.set_xlabel('Hours (Midnight - Midnight)')
+ax1.legend(loc='upper left')
+ax1.set_title('Average Hourly Volumes - Richards Hall with error bars.  Hours(x-axis) vs Total Gallons Used(y-axis)',
+              fontsize=14)
 
+# Plot as boxplot to see range, mean, and IQR.
 
+# Convert data to list.  More efficient to plot as two boxplots in same
+# Axes since boxplot converts a 2-D array into list of vectors anyway.
+data = [week_hourlyAvgVol, weekend_hourlyAvgVol]
+ax2.boxplot(x=data, notch=True, showmeans=True)
+ax2.set_title('Consumption Range', fontsize=8)
+ax2.set_xlabel('Week (1), Weekend (2)')
+ax2.set_ylabel('Gallons Consumed')
 
+# Plot as histogram to see frequency of consumption in hourly time frame.
+data2 = [week_hourlyTotVol, weekend_hourlyTotVol]
+ax3.hist(data, label=('Week (Mon-Fri)', 'Weekend (Sat-Sun)'))
+ax3.legend(loc='upper left')
+ax3.set_title('Freq. of Consumption Intervals', fontsize = 8)
+ax3.set_ylabel('Frequency')
+ax3.set_xlabel('Gallons Consumed')
 
-
-
+date1 = '2017-03-07 13:39:00'
+date2 = '2017-03-17 13:20:00'
+date3 = '2017-03-20 09:51:00'
+date4 = ''
 
 plt.show()
 
--- source_6_2.py
+++ source_6_2.py
@@ -3,6 +3,7 @@
 # 1. import packages
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.tools.plotting import table
 
 # 2. read csv from Richards Hall and load data into pandas MAIN data frame
 df_main = pd.read_csv('datalog_Richards_Hall.csv', header =1, sep=',',index_col=0,
@@ -17,12 +18,12 @@ beginDate = '2017-03-03 15:13:00'
 endDate = '2017-03-27 16:20:00'
 
 
-
 df_resample = df_main[beginDate:endDate].copy()
 df_manual = df_manual[beginDate:endDate].copy()
 
 # 5. Subsample full 1s resolution into 30s, 1m, 1h, 24h.
 
+df_1s = df_resample
 df_5s = df_resample[0::5].copy()
 df_10s = df_resample[0::10].copy()
 df_30s = df_resample[0::30].copy()
@@ -31,28 +32,49 @@ df_5s['TotVol'] = df_5s['IncrementalVolume'].cumsum()
 df_10s['TotVol'] = df_10s['IncrementalVolume'].cumsum()
 df_30s['TotVol'] = df_30s['IncrementalVolume'].cumsum()
 
-
-# df_1sSum = df_resample['IncrementalVolume'].sum()
-# df_5sSum = df_5s['IncrementalVolume'].sum()
-# df_10sSum = df_10s['IncrementalVolume'].sum()
-# df_30sSum = df_30s['IncrementalVolume'].sum()
+last =[['108484 gal', '21696 gal', '10849 gal', '3604 gal', '110248 gal']]
+df_summary = pd.DataFrame(last, columns=('1s total', '5s total', '10s total', '30s total', 'manual total'))
 
 # 6. plot stuff
 
 # plot incremental volume
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
+#fig = plt.figure()
+#ax = fig.add_subplot(1,1,1)
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(10,6))
 
-df_resample.plot(y='TotalizedVolume', ax=ax, kind='line', use_index=True,
-                label = '1s Data')
-df_5s.plot(y='TotVol', ax=ax, kind='line', use_index=True,
-            label = '5s Data')
-df_10s.plot(y='TotVol', ax=ax, kind='line', use_index=True,
-            label = '10s Data')
-df_30s.plot(y='TotVol', ax=ax, kind='line', use_index=True,
-            label = '30s Data')
-df_manual.plot(y='Volume_Used_gal', ax=ax, kind='line', use_index=True,
-                marker = 'o', label = 'Manual Meter')
+ax1.plot(df_1s['TotalizedVolume'], label='1s')
+ax1.plot(df_5s['TotVol'], label='5s')
+ax1.plot(df_10s['TotVol'], label='10s')
+ax1.plot(df_30s['TotVol'], label='30s')
+ax1.plot(df_manual['Volume_Used_gal'], marker='o', label='Manual readings')
+ax1.set_title('Cumulative Volume with Decimated Data Resolution: 1s, 5, 10s, 30s, & Manual\n '
+              'Total Cumulative Volume displayed below')
+ax1.set_xlabel('Date (MST)')
+ax1.set_ylabel('Cumulative Volume (gal)')
+
+
+ax2.table(cellText =df_summary.values, colLabels=df_summary.columns, loc='upper center')
+ax2.axis('tight')
+ax2.axis('off')
+ax2.set_ylabel('Total Cumulative Volume')
+
+
+
+
+
+#df_1s.plot(y='TotalizedVolume', ax=ax, kind='line', use_index=True,
+#            label = '1s Data')
+#df_5s.plot(y='TotVol', ax=ax, kind='line', use_index=True,
+#            label = '5s Data')
+#df_10s.plot(y='TotVol', ax=ax, kind='line', use_index=True,
+#            label = '10s Data')
+#df_30s.plot(y='TotVol', ax=ax, kind='line', use_index=True,
+#            label = '30s Data')
+#df_manual.plot(y='Volume_Used_gal', ax=ax, kind='line', use_index=True,
+#                marker = 'o', label = 'Manual Meter')
+#ax.set_title('Cumulative Volume with Decimated Data Resolution: 1s, 5, 10s, 30s, & Manual')
+#ax.set_xlabel('Date (MST)')
+#ax.set_ylabel('Cumulative Volume (gal)')
 
 plt.show()
 
